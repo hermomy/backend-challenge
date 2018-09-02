2a0bf69064deecef534c53a527aae52d26456ee7
eviltwin <berushacker@gmail.com>
2018-09-03 06:38:22 +0800

    START

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","hermomy");
+$con = mysqli_connect("localhost","root","","hermomy2");
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
