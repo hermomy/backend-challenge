27cc681ade72c897b8e6b1c019262037964185dc
eviltwin <berushacker@gmail.com>
2018-09-03 08:21:12 +0800

    END
    awaiting pull request

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,6 @@
 <?php
 
 $con = mysqli_connect("localhost","root","","hermomy");
-$db = mysqli_select_db($con,"hermomy")or die("not select");
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
